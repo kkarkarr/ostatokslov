e5988f1ddccdb12536b73404cd4cd2264af6bd25
kkarkarr <lasvegas2992@gmail.com>
2023-04-09 16:36:43 +0300

    user.model.js was done

--- src/resources/users/user.model.js
+++ src/resources/users/user.model.js
@@ -14,4 +14,55 @@ class User {
   }
 }
 
+class Player {
+  constructor({ id = uuidv4(), name = 'USER', surname = 'SURNAME', nickname = 'NICKNAME', teamId = uuidv4(), country = 'COUNTRY', references = 'REFERENCES' , age = 'AGE'    } = {}) {
+    this.id = id;
+    this.name = name;
+    this.surname = surname;
+    this.nickname = nickname;
+    this.teamId = teamId;
+    this.country = country;
+    this.references = references;
+    this.age = age;
+  }
+
+  static toResponse(player) {
+    const { id, name, surname, nickname, teamId } = player;
+    return { id, name, surname, nickname, teamId };
+  }
+}
+
+class Team {
+  constructor({ id = uuidv4(), name = 'TEAM', region = 'REGION', country = 'COUNTRY', trophyId = uuidv4(), playerId = uuidv4()} = {}) {
+    this.id = id;
+    this.name = name;
+    this.region = region;
+    this.country = country;
+    this.trophyId = trophyId;
+    this.playerId = playerId;
+
+  }
+
+  static toResponse(team) {
+    const { id, name, region, country, trophyId, playerId } = team ;
+    return { id, name, region, country, trophyId, playerId };
+  }
+}
+
+class Trophy {
+  constructor({ id = uuidv4(), name = 'TROPHY', place = 'PLACE', country = 'COUNTRY', winner = 'WINNER', reward = 'REWARD'} = {}) {
+    this.id = id;
+    this.name = name;
+    this.place = place;
+    this.country = country;
+    this.winner = winner;
+
+  }
+
+  static toResponse(trophy) {
+    const { id, name, place, country, winner } = trophy ;
+    return { id, name, place, country, winner };
+  }
+}
+
 export default User;
